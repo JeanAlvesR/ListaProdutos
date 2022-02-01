ffe6f15bf43ff3ba5f2ea059761220906acd515d
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-02-01 15:51:17 -0300

    add filtro
    add filtro

--- js/script1.js
+++ js/script1.js
@@ -1,48 +1,49 @@
-class Produto{
+class Produto {
 
-    constructor(){
+    constructor() {
         this.codigoProduto = 1;
         this.arrayProdutos = [];
 
     }
 
-    salvar(){
+    salvar() {
+
         let produto = this.lerDados();
-       if( this.validaCampos(produto)){
+        if (this.validaCampos(produto)) {
             alert('Salvar');
             this.adicionar(produto);
-       }
+        }
         this.listaTabela();
-        
+
         this.cancelar();
     }
 
-    listaTabela(){
+    listaTabela() {
         let tbody = document.getElementById('tbody');
 
         tbody.innerText = '';
 
-        for(let i = 0; i<this.arrayProdutos.length; i++){
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
             let tr = tbody.insertRow();
 
             let td_codigo = tr.insertCell();
             let td_nome = tr.insertCell();
             let td_preco = tr.insertCell();
-            
+
 
             td_codigo.innerText = this.arrayProdutos[i].codigoProduto;
             td_nome.innerText = this.arrayProdutos[i].nomeProduto;
             td_preco.innerText = this.arrayProdutos[i].precoProduto;
-         
+
         }
     }
 
-    adicionar(produto){
+    adicionar(produto) {
         this.arrayProdutos.push(produto);
         this.codigoProduto++;
     }
 
-    lerDados(){
+    lerDados() {
         let produto = {}
         produto.codigoProduto = this.codigoProduto;
         produto.nomeProduto = document.getElementById('produto').value;
@@ -51,16 +52,16 @@ class Produto{
         return produto;
     }
 
-    validaCampos(produto){
+    validaCampos(produto) {
         let msg = '';
 
-        if(produto.nomeProduto == ''){
+        if (produto.nomeProduto == '') {
             msg += '- Informe o nome do Produto \n';
         }
-        if(produto.precoProduto == ''){
+        if (produto.precoProduto == '') {
             msg += '- Informe o Preço do Produto \n';
         }
-        if(msg!=''){
+        if (msg != '') {
             alert(msg);
             return false;
         }
@@ -68,10 +69,33 @@ class Produto{
         return true;
     }
 
-    cancelar(){
+    filtrar() {
+        let nome = document.getElementById('produto').value;
+        if (nome == '') {
+            alert('Inserir nome para verificar!!!')
+        }
+        else {
+            let verifica = true;
+            for (let i = 0; i < this.arrayProdutos.length; i++) {
+                if (this.arrayProdutos[i].nomeProduto == nome) {
+                    alert('Código: ' + this.arrayProdutos[i].codigoProduto + '\nNome: ' + this.arrayProdutos[i].nomeProduto + '\nPreço: ' + this.arrayProdutos[i].precoProduto);
+                    verifica = false;
+                }
+            }
+            if (verifica) {
+                alert('Não existe esse nome na lista!');
+            }
+        }
+
+        this.cancelar();
+
+    }
+
+    cancelar() {
         document.getElementById('produto').value = '';
         document.getElementById('preco').value = '';
     }
+
 }
 
 var produto = new Produto();
