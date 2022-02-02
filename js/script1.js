615ce271667b5d2fd4b99171ebeb0ea7c6b68e57
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-02-02 13:37:24 -0300

    Update
    Add array auxiliar, botão de listar todos e ordenação na filtragem.

--- js/script1.js
+++ js/script1.js
@@ -1,8 +1,10 @@
 class Produto {
-
+    
     constructor() {
+
         this.codigoProduto = 1;
         this.arrayProdutos = [];
+        this.arrayProdutosAuxiliar = [];
 
     }
 
@@ -13,17 +15,17 @@ class Produto {
             alert('Salvar');
             this.adicionar(produto);
         }
-        this.listaTabela();
+        this.listaTabela(this.arrayProdutos);
 
         this.cancelar();
     }
 
-    listaTabela() {
+    listaTabela(arrayProdutos) {
         let tbody = document.getElementById('tbody');
 
         tbody.innerText = '';
 
-        for (let i = 0; i < this.arrayProdutos.length; i++) {
+        for (let i = 0; i < arrayProdutos.length; i++) {
             let tr = tbody.insertRow();
 
             let td_codigo = tr.insertCell();
@@ -31,20 +33,21 @@ class Produto {
             let td_preco = tr.insertCell();
 
 
-            td_codigo.innerText = this.arrayProdutos[i].codigoProduto;
-            td_nome.innerText = this.arrayProdutos[i].nomeProduto;
-            td_preco.innerText = this.arrayProdutos[i].precoProduto;
+            td_codigo.innerText = arrayProdutos[i].codigoProduto;
+            td_nome.innerText = arrayProdutos[i].nomeProduto;
+            td_preco.innerText = arrayProdutos[i].precoProduto;
 
         }
     }
 
     adicionar(produto) {
         this.arrayProdutos.push(produto);
+        this.arrayProdutosAuxiliar.push(produto);
         this.codigoProduto++;
     }
 
     lerDados() {
-        let produto = {}
+        let produto = {};
         produto.codigoProduto = this.codigoProduto;
         produto.nomeProduto = document.getElementById('produto').value;
         //quando há a conversão, caso a pessoa insira o número com ',', a informação após a virgula era ignorada. O replace corrige isso.
@@ -80,20 +83,15 @@ class Produto {
             let tbody = document.getElementById('tbody');
             tbody.innerText = '';
             let verifica = true;
+            this.arrayProdutosAuxiliar = [];
             for (let i = 0; i < this.arrayProdutos.length; i++) {
                 if (this.arrayProdutos[i].nomeProduto == nome) {
-                    let tr = tbody.insertRow();
-                    let td_codigo = tr.insertCell();
-                    let td_nome = tr.insertCell();
-                    let td_preco = tr.insertCell();
-
-                    td_codigo.innerText = this.arrayProdutos[i].codigoProduto;
-                    td_nome.innerText = this.arrayProdutos[i].nomeProduto;
-                    td_preco.innerText = this.arrayProdutos[i].precoProduto;
+                    this.arrayProdutosAuxiliar.push(this.arrayProdutos[i]);
 
                     verifica = false;
                 }
             }
+            this.listaTabela(this.arrayProdutosAuxiliar);
             if (verifica) {
                 alert('Não existe esse nome na lista!');
             }
@@ -108,32 +106,48 @@ class Produto {
         document.getElementById('preco').value = '';
     }
 
+    
     organizar(parametro) {
-
-        if (parametro) {
-            for (let i = 0; i < this.arrayProdutos.length; i++) {
-                for (let j = 0; j < this.arrayProdutos.length; j++) {
-                    if (this.arrayProdutos[i].precoProduto > this.arrayProdutos[j].precoProduto) {
-                        let temp = Object.assign({}, this.arrayProdutos[i]);
-                        this.arrayProdutos[i] = Object.assign({}, this.arrayProdutos[j]);
-                        this.arrayProdutos[j] = Object.assign({}, temp);
+        
+        //decrescente
+        if (parametro === 1) {
+            
+            for (let i = 0; i < this.arrayProdutosAuxiliar.length; i++) {
+                for (let j = 0; j < this.arrayProdutosAuxiliar.length; j++) {
+                    if (this.arrayProdutosAuxiliar[i].precoProduto > this.arrayProdutosAuxiliar[j].precoProduto) {
+                        let temp = Object.assign({}, this.arrayProdutosAuxiliar[i]);
+                        this.arrayProdutosAuxiliar[i] = Object.assign({}, this.arrayProdutosAuxiliar[j]);
+                        this.arrayProdutosAuxiliar[j] = Object.assign({}, temp);
                     }
                 }
             }
         }
-        else {
-            for (let i = 0; i < this.arrayProdutos.length; i++) {
-                for (let j = 0; j < this.arrayProdutos.length; j++) {
-                    if (this.arrayProdutos[i].precoProduto < this.arrayProdutos[j].precoProduto) {
-                        let temp = Object.assign({}, this.arrayProdutos[i]);
-                        this.arrayProdutos[i] = Object.assign({}, this.arrayProdutos[j]);
-                        this.arrayProdutos[j] = Object.assign({}, temp);
+        //crescente
+        else if(parametro === 0) {
+            for (let i = 0; i < this.arrayProdutosAuxiliar.length; i++) {
+                for (let j = 0; j < this.arrayProdutosAuxiliar.length; j++) {
+                    if (this.arrayProdutosAuxiliar[i].precoProduto < this.arrayProdutosAuxiliar[j].precoProduto) {
+                        let temp = Object.assign({}, this.arrayProdutosAuxiliar[i]);
+                        this.arrayProdutosAuxiliar[i] = Object.assign({}, this.arrayProdutosAuxiliar[j]);
+                        this.arrayProdutosAuxiliar[j] = Object.assign({}, temp);
                     }
                 }
             }
         }
-        this.listaTabela();
+        else{
+            this.copiaArray();
+        }
+        this.listaTabela(this.arrayProdutosAuxiliar);
     }
+
+    copiaArray(){
+        this.arrayProdutosAuxiliar = [];
+        for(let i = 0; i<this.arrayProdutos.length; i++){
+            this.arrayProdutosAuxiliar.push(this.arrayProdutos[i]);
+        }
+    }
+
+    
 
 }
 
